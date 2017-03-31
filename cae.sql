0b9db26987917bda257ffe82f4034ee4d31008ad
kuma <709782571@qq.com>
2017-03-31 21:15:58 +0800

    增加song表的设计，并依次调整各个接口和call表结构

--- cae.sql
+++ cae.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50619
 File Encoding         : 65001
 
-Date: 2017-03-29 11:22:12
+Date: 2017-03-31 21:14:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,11 +21,24 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `call_record`;
 CREATE TABLE `call_record` (
   `call_id` varchar(10) NOT NULL,
-  `call_name` varchar(20) NOT NULL,
-  `call_content` varchar(50) NOT NULL,
-  `call_sell_time` date NOT NULL,
+  `song_id` varchar(10) NOT NULL,
+  `call_source` varchar(50) NOT NULL,
   `call_version` smallint(6) NOT NULL,
-  `call_cover` varchar(50) NOT NULL,
   `call_last_modify` timestamp NOT NULL,
-  PRIMARY KEY (`call_id`)
+  PRIMARY KEY (`call_id`),
+  KEY `song_id` (`song_id`),
+  CONSTRAINT `call_record_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `song` (`song_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for song
+-- ----------------------------
+DROP TABLE IF EXISTS `song`;
+CREATE TABLE `song` (
+  `song_id` varchar(10) NOT NULL,
+  `song_name` varchar(20) NOT NULL,
+  `song_sell_time` date NOT NULL,
+  `song_owner` varchar(10) NOT NULL,
+  `song_cover` varchar(50) NOT NULL,
+  PRIMARY KEY (`song_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
